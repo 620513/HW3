42dfa138093288eab016e658e6163e607b125385
ThelmaAndrews <thelma.andrews@calbaptist.edu>
2016-10-01 19:45:44 -0700

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main (String args[])
     {
         Triple <String,String,String> T = new Triple<>("left", "middle","right");
-        Triple <String,Double,Integer>  K= new Triple<>("left",7.0,4);
+        Triple <String,Double,Integer>  K= new Triple<>("left",7.0,8);
         System.out.println(T);
         System.out.println(K);
     }
